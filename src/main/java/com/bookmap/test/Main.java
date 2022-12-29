4d02ff6b95a4d21a1d968f921276061fb4ff1792
AntonPav <streetreadger@gmail.com>
2022-12-29 18:37:19 +0200

    without output file

--- src/main/java/com/bookmap/test/Main.java
+++ src/main/java/com/bookmap/test/Main.java
@@ -4,31 +4,32 @@ import com.bookmap.test.service.StrategyManager;
 import com.bookmap.test.service.impl.OperationManagerImpl;
 import com.bookmap.test.service.impl.OutputServiceImpl;
 import com.bookmap.test.service.impl.StrategyManagerImpl;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
 
 public class Main {
     private static final String INPUT_FILE_PATH = "input.txt";
     private static final String OUTPUT_FILE_PATH = "output.txt";
 
     public static void main(String[] args) {
-        List<String> strings = readFile(INPUT_FILE_PATH);
         prepareOutputFile(OUTPUT_FILE_PATH);
-
-        StrategyManager lineStrategy = new StrategyManagerImpl(
-                new OperationManagerImpl(),
-                new OutputServiceImpl(OUTPUT_FILE_PATH));
-        strings.forEach(lineStrategy::lineStrategy);
-    }
-
-    private static List<String> readFile(String filePath) {
-        File file = new File(filePath);
-        try {
-            return Files.readAllLines(file.toPath());
+        String line;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(INPUT_FILE_PATH));
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(OUTPUT_FILE_PATH))) {
+            StrategyManager lineStrategy = new StrategyManagerImpl(
+                    new OperationManagerImpl(),
+                    new OutputServiceImpl(bufferedWriter));
+            System.out.println();
+            while ((line = bufferedReader.readLine()) != null) {
+                lineStrategy.lineStrategy(line);
+            }
         } catch (IOException e) {
-            throw new RuntimeException("Can't read input file " + INPUT_FILE_PATH, e);
+            throw new RuntimeException(e);
         }
     }
 
