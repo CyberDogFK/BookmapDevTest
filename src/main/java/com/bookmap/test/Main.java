2c4a30cb93f09fe179183896d4889cce5434b8b4
AntonPav <streetreadger@gmail.com>
2023-01-03 11:27:56 +0200

    im given up

--- src/main/java/com/bookmap/test/Main.java
+++ src/main/java/com/bookmap/test/Main.java
@@ -9,6 +9,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,20 +22,45 @@ public class Main {
 
     public static void main(String[] args) {
         prepareOutputFile(OUTPUT_FILE_PATH);
+        readAllLineByLine();
+    }
+
+    // better perfomance in small files
+//    // need change StrategyManager parameters to StringBuilder
+//    private static void readAllInMemory() {
+//        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of(OUTPUT_FILE_PATH))) {
+//            StrategyManager strategyManager = new StrategyManagerImpl(
+//                    new OperationManagerImpl(),
+//                    new OutputServiceImpl(bufferedWriter));
+//        byte[] bytes =  Files.readAllBytes(Path.of(INPUT_FILE_PATH));
+//
+//        StringBuilder stringBuilder = new StringBuilder();
+//            for (int i = 0; i < bytes.length; i++) {
+//                stringBuilder.setLength(0);
+//                while (bytes[i] != System.lineSeparator().charAt(0)) {
+//                    stringBuilder.append((char) bytes[i]);
+//                    i++;
+//                    if (i >= bytes.length) {
+//                        break;
+//                    }
+//                }
+//                strategyManager.lineStrategy(stringBuilder);
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
+    //better perfomance for huge files
+    private static void readAllLineByLine() {
         String line;
         try (BufferedReader bufferedReader = Files.newBufferedReader(Path.of(INPUT_FILE_PATH));
              BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of(OUTPUT_FILE_PATH))) {
             StrategyManager strategyManager = new StrategyManagerImpl(
                     new OperationManagerImpl(),
                     new OutputServiceImpl(bufferedWriter));
-            int count = 0;
             while ((line = bufferedReader.readLine()) != null) {
-                strategyManager.lineStrategy(line);
-                count++;
-                if (count >= 100_000) {
-                    System.gc();
-                    count = 0;
-                }
+                strategyManager.lineStrategy(line); // need change StrategyManager parameters to String
             }
 
 //            byte[] bytes = Files.readAllBytes(Path.of(INPUT_FILE_PATH));
